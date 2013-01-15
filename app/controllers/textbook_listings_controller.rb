2c823f9f8913266ff06c33866c1900b45d7d314a
Albert <albertkoy@gmail.com>
2013-01-15 01:13:02 -0500

    add error partial

--- app/controllers/textbook_listings_controller.rb
+++ app/controllers/textbook_listings_controller.rb
@@ -8,6 +8,17 @@ class TextbookListingsController < ApplicationController
   def update
   end
 
+  def create
+    params[:textbook_listing][:isbn] = params[:isbn]
+    @textbook_listing = TextbookListing.new(params[:textbook_listing])
+    puts @textbook_listing.inspect
+    if @textbook_listing.save
+      redirect_to @textbook_listing
+    else
+      render 'pages/sell'
+    end
+  end
+
   def show
     @textbook_listing = TextbookListing.find(params[:id])
   end
